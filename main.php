60f4ebff4b989e6fce1668a0d00bedb77173ef12
hermoine <vehllej@gmail.com>
2025-08-13 01:41:49 +0000

    Act2

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+$Name = "hermoine";
+$Age = "19";
+$School = "Global reciprocal Colleges";
+$Course = "BSIT";
+$subject = "Websys";
+$isstudent = "Yes";
+
+
+echo "Name: $Name<br>";
+echo "Age:  $Age<br>";
+echo "School:  $School<br>";
+echo "Course:  $Course<br>";
+echo "Subject: $subject<br>";
+echo "Student?:  $isstudent<br>";
+
+?>
